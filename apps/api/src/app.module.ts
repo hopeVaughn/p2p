aae0657a52eeb3d071fdbc75386b04ee9e8a64f4
hopeVaughn <hopevaughnwarren@gmail.com>
2023-06-29 12:25:14 -0700

    adjust all package.json scripts to allow for build and deploy

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'client', 'dist'), // <-- path to the static files
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
