7f05a17a42cdef3411862f5bd52a07a1e4165c8a
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-05 08:35:15 -0700

    update turbo dependency. update port number for nest to eliminate potential port conflicts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api'); // <--- this line set the route prefix
-  await app.listen(3000);
+  await app.listen(3333);
 }
 bootstrap();
