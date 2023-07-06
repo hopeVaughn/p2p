85fe01b69420a4e231bb60235214cec9d0aafcb6
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-06 10:45:31 -0700

    add auth folder and auth module. add auth module to app.module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'client', 'dist'), // <-- path to the static files
     }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
