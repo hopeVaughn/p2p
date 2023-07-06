9b6cc39b8320bef1ae8a1c2e46d7f700ebbfe27d
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-06 10:15:24 -0700

    remove uneccessary boiler plate from api

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,17 +1,8 @@
-import { useEffect, useState } from "react";
 
 function App() {
-  const [greeting, setGreeting] = useState<string>('');
-
-  useEffect(() => {
-    fetch('/api')
-      .then((res) => res.text())
-      .then(setGreeting);
-  }, []);
-
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="font-bold">{greeting}</h1>
+      <h1 className="font-bold">Hello World!</h1>
     </div>
   );
 }
