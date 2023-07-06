919200ebc5d05738cff8cd5d7c1954387a333f1c
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-06 10:22:47 -0700

    adjust app.module.ts for dev delivery

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { join } from 'path';
 
 @Module({
@@ -10,7 +8,7 @@ import { join } from 'path';
       rootPath: join(__dirname, '../..', 'client', 'dist'), // <-- path to the static files
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
