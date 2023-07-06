fddc2cc32c8a9ae28e707ab0f3527fa36a773c82
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-06 11:08:04 -0700

    use nest cli to generate user and bathroom modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BathroomModule } from './bathroom/bathroom.module';
 
 @Module({
   imports: [
@@ -9,6 +11,8 @@ import { AuthModule } from './auth/auth.module';
       rootPath: join(__dirname, '../..', 'client', 'dist'), // <-- path to the static files
     }),
     AuthModule,
+    UserModule,
+    BathroomModule,
   ],
   controllers: [],
   providers: [],
