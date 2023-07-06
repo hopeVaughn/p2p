7a3d58573c1c36fa6be66b31e0f32e8b6562ebf9
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-06 11:52:45 -0700

    update auth.service and auth.controller for initial testing

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller()
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  // api/auth/signup
+  @Post('signup')
+  signup() {
+    return 'I am a new user signing up';
+  }
+
+  // api/auth/signin
+  @Post('signin')
+  signin() {
+    return 'I am a user signing in';
+  }
+}
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  login() {
+    // do something
+  }
+
+  signup() {
+    // do something
+  }
+}
