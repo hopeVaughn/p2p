87459c5fc2c3b979313ddf70743411d62a8da2ae
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-07 08:01:03 -0700

    update .env add variable for future jwt dependency. add try catch and validation handler to auth.service to check for duplicate email

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
@@ -9,18 +11,27 @@ export class AuthService {
     // generate password
     const hash = await argon.hash(dto.password);
     // save user to db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        password: hash,
-      },
-    });
-    delete user.password;
-    // return saved user
-    return user;
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+      delete user.password;
+      // return saved user
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
   }
 
-  signin() {
+  async signin() {
     return { message: 'I am a user signing in' };
   }
 }
