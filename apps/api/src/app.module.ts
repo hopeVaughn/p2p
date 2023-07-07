2bab8ec91777c572d6e64a6e904e0c099b04b81f
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-07 08:47:27 -0700

    add @nestjs/config and change hard code value to .env value using config dependency

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
@@ -13,6 +14,9 @@ import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'client', 'dist'), // <-- path to the static files
     }),
