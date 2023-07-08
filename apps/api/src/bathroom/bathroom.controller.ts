d86091db1180480b84e8057ea76351716bd1577e
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-08 11:33:47 -0700

    add bathroom module, controller, and services to apps/api and preped for initial api testing

---
+++ apps/api/src/bathroom/bathroom.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { BathroomService } from './bathroom.service';
+import { CreateBathroomDto, UpdateBathroomDto } from './dto/bathroom.dto';
+import { JwtAuthGuard } from '../auth/guard/jwt.guard';
+
+@Controller('bathroom')
+export class BathroomController {
+  constructor(private readonly bathroomService: BathroomService) {}
+
+  // Create a new bathroom. This route is protected, and only authenticated users can access it.
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createBathroomDto: CreateBathroomDto) {
+    return this.bathroomService.create(createBathroomDto);
+  }
+
+  // Get a list of all bathrooms. This route is public.
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll() {
+    return this.bathroomService.findAll();
+  }
+
+  // Get a specific bathroom by its ID. This route is public.
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  findOne(@Param('id') id: string) {
+    return this.bathroomService.findOne(id);
+  }
+
+  // Update a specific bathroom. This route is protected, and only authenticated users can access it.
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  update(
+    @Param('id') id: string,
+    @Body() updateBathroomDto: UpdateBathroomDto,
+  ) {
+    return this.bathroomService.update(id, updateBathroomDto);
+  }
+
+  // Delete a specific bathroom. This route is protected, and only authenticated users can access it.
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return this.bathroomService.remove(id);
+  }
+}
---
+++ apps/api/src/bathroom/bathroom.service.ts
@@ -0,0 +1,75 @@
+import {
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateBathroomDto, UpdateBathroomDto } from './dto/bathroom.dto';
+
+@Injectable()
+export class BathroomService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createBathroomDto: CreateBathroomDto) {
+    try {
+      return await this.prisma.bathroom.create({
+        data: {
+          ...createBathroomDto,
+          createdBy: {
+            connect: {
+              id: createBathroomDto.createdBy,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException('Error during bathroom creation');
+    }
+  }
+
+  async findAll() {
+    try {
+      return await this.prisma.bathroom.findMany();
+    } catch (error) {
+      throw new InternalServerErrorException('Error retrieving bathrooms');
+    }
+  }
+
+  async findOne(id: string) {
+    try {
+      const bathroom = await this.prisma.bathroom.findUnique({ where: { id } });
+      if (!bathroom) throw new NotFoundException('Bathroom not found');
+      return bathroom;
+    } catch (error) {
+      if (error instanceof NotFoundException) throw error;
+      throw new InternalServerErrorException('Error retrieving the bathroom');
+    }
+  }
+
+  async update(id: string, updateBathroomDto: UpdateBathroomDto) {
+    try {
+      const bathroom = await this.prisma.bathroom.findUnique({ where: { id } });
+      if (!bathroom) throw new NotFoundException('Bathroom not found');
+
+      return await this.prisma.bathroom.update({
+        where: { id },
+        data: updateBathroomDto,
+      });
+    } catch (error) {
+      if (error instanceof NotFoundException) throw error;
+      throw new InternalServerErrorException('Error updating the bathroom');
+    }
+  }
+
+  async remove(id: string) {
+    try {
+      const bathroom = await this.prisma.bathroom.findUnique({ where: { id } });
+      if (!bathroom) throw new NotFoundException('Bathroom not found');
+
+      return await this.prisma.bathroom.delete({ where: { id } });
+    } catch (error) {
+      if (error instanceof NotFoundException) throw error;
+      throw new InternalServerErrorException('Error deleting the bathroom');
+    }
+  }
+}
