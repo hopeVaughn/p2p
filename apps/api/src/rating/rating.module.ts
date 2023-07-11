2d7bdabbd7480b7adcbe229b9b1dd86bf95fae0d
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-11 11:58:59 -0700

    adjust rating.module for export

--- apps/api/src/rating/rating.module.ts
+++ apps/api/src/rating/rating.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RatingService } from './rating.service';
+import { RatingController } from './rating.controller';
 
-@Module({})
+@Module({
+  controllers: [RatingController],
+  providers: [RatingService],
+})
 export class RatingModule {}
