a544f3ad10ce946f2264acf98199a4599a189403
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-18 09:03:42 -0700

    update role.dto

--- apps/api/src/role/dto/role.dto.ts
+++ apps/api/src/role/dto/role.dto.ts
@@ -1,10 +1,11 @@
-import { IsNotEmpty, IsEnum } from 'class-validator';
+import { IsNotEmpty, IsEnum, IsUUID } from 'class-validator';
 import { RoleName } from '@prisma/client';
 
 // This DTO validates that the id field is not empty and name field is one of the values from the RoleName enum.
-export class RoleDto {
+export class AssignRoleDto {
   @IsNotEmpty()
-  id: string;
+  @IsUUID()
+  userUuid: string;
 
   @IsEnum(RoleName)
   name: RoleName;
