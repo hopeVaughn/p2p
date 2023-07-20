456e50a1dd0397dfa99adef1e41ec893a4a5e3fd
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-20 14:28:16 -0700

    adjust import to fix server error

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { UserDto } from './dto/user.dto';
+// import { UserDto } from './dto/user.dto';
 
 @Injectable()
 export class UserService {
