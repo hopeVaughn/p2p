75a07668e35ac3ef9e03a33c58a401780ce8e549
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-26 19:47:09 -0700

    update formrow and formbackground to be responsive

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,12 +1,36 @@
 import Button from "./Global_Components/Buttons";
+import FormRow from "./Global_Components/FormRow";
+import FormBackdrop from "./Global_Components/FormBackdrop";
 function App() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Button
-        variant="primary"
-        btnText="default booper"
-      />
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Handle form submission here
+  };
 
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // Handle input changes here
+  };
+  return (
+    <div className="flex justify-center items-center h-screen bg-orange-100">
+      <FormBackdrop onSubmit={handleFormSubmit}>
+        <FormRow
+          type="text"
+          name="name"
+          value=""
+          handleChange={handleInputChange}
+          labelText="Name:"
+          placeholder="Hope Warren"
+        />
+        <FormRow
+          type="email"
+          name="email"
+          value=""
+          handleChange={handleInputChange}
+          labelText="Email:"
+          placeholder="hope@email.com"
+        />
+        {/* Add more form rows as needed */}
+      </FormBackdrop>
     </div>
   );
 }
