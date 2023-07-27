7a5abc3cd0e80a184afa3dac669c1a96917fc0ef
hopeVaughn <hopevaughnwarren@gmail.com>
2023-07-27 08:17:50 -0700

    update nav bar to be responsive to screen sizes

--- apps/client/src/Global_Components/Navbar.tsx
+++ apps/client/src/Global_Components/Navbar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import Logo from '../assets/Logo.svg';  // adjust path to your actual Logo path
+import Banner from '../assets/Icons/Banner.svg';
+import Logo_Main from '../assets/Icons/Logo_Main.svg';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="relative w-full z-50">
-      <img src={Logo} alt="Logo" className="fixed top-0 left-0 h-auto w-full z-10 pr-4" />
+    <nav className="relative w-full h-16 z-50">
+      <img src={Banner} alt="Banner" className="absolute top-0 left-0 h-16 w-full z-10" />
+
+      <div className="absolute top-0 flex items-center justify-start h-full w-full z-20 px-6">
+        <img src={Logo_Main} alt="Logo" style={{ position: 'fixed', width: '25vw', height: '15vw', left: '1vw' }} />
+      </div>
     </nav>
   );
 };
