240071092982e3a12b4a25f3c236defbac334625
hopeVaughn <hopevaughnwarren@gmail.com>
2023-08-01 10:50:15 -0700

    change font size for error pages

--- apps/client/src/Pages/Error.tsx
+++ apps/client/src/Pages/Error.tsx
@@ -4,7 +4,7 @@ import { server_down } from '../assets';
 const Error: React.FC = () => {
   return (
     <main className='flex flex-col items-center justify-center bg-gray-100 min-h-screen text-center md:px-0 relative'>
-      <h1 className='text-6xl text-teal-800 mt-4 text-center pb-10 w-full'>Somethings Wrong!</h1>
+      <h1 className='text-5xl text-teal-800 mt-4 text-center pb-10 w-full'>Somethings Wrong!</h1>
       <img src={server_down} className='mx-auto max-w-full h-[65vh]' alt="server_down" />
       <p className='text-4xl md:py-16 text-teal-900 text-center w-full'>We're working on it!
         <a href="#" className='pl-4 text-3xl text-teal-600 text-center w-full'>Go Back</a>
