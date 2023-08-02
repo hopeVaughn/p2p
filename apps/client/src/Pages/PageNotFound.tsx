3a9c6fa5006c78670debac6724242d68ebaee931
hopeVaughn <hopevaughnwarren@gmail.com>
2023-08-02 11:41:44 -0700

    add search boiler and adjust color for 404 page

--- apps/client/src/Pages/PageNotFound.tsx
+++ apps/client/src/Pages/PageNotFound.tsx
@@ -3,7 +3,7 @@ import { page_not_found } from '../assets';
 
 const PageNotFound: React.FC = () => {
   return (
-    <main className='flex flex-col items-center justify-center bg-gray-100 min-h-screen text-center md:px-0 relative'>
+    <main className='flex flex-col items-center justify-center bg-orange-100 min-h-screen text-center md:px-0 relative'>
       <h1 className='text-5xl  text-teal-800 mt-4 text-center pb-10 w-full'>This page doesn't exist</h1>
       <img src={page_not_found} className='mx-auto max-w-full h-[65vh]' alt="age_not_found" />
       <p className='text-4xl md:py-16 text-teal-900 text-center w-full'>It happens....
---
+++ apps/client/src/Pages/Protected_Routes/Search.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../Global_Components/Navbar'
+import Sidebar from '../../Global_Components/Sidebar'
+
+const Search: React.FC = () => {
+  return (
+    <div>Search</div>
+  )
+}
+
+export default Search
