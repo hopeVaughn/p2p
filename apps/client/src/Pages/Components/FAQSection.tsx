bf88f18d707087e602656aac666bd4f32396a1b6
hopeVaughn <hopevaughnwarren@gmail.com>
2023-10-04 10:31:34 -0700

    add rework components

---
+++ apps/client/src/Pages/Components/FAQSection.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+interface FAQ {
+  question: string;
+  answer: string;
+}
+
+const faqs: FAQ[] = [
+  {
+    question: 'How do you make holy water?',
+    answer:
+      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+  },
+  // More questions...
+];
+
+export default function FAQSection() {
+  return (
+    <section className="bg-white">
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:pt-32 lg:px-8 lg:py-40">
+        <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+          <aside className="lg:col-span-5">
+            <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">Frequently asked questions</h2>
+            <p className="mt-4 text-base leading-7 text-gray-600">
+              Can’t find the answer you’re looking for? Reach out to our{' '}
+              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                customer support
+              </a>{' '}
+              team.
+            </p>
+          </aside>
+          <article className="mt-10 lg:col-span-7 lg:mt-0">
+            <dl className="space-y-10">
+              {faqs.map((faq) => (
+                <React.Fragment key={faq.question}>
+                  <dt className="text-base font-semibold leading-7 text-gray-900">{faq.question}</dt>
+                  <dd className="mt-2 text-base leading-7 text-gray-600">{faq.answer}</dd>
+                </React.Fragment>
+              ))}
+            </dl>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+}
+
