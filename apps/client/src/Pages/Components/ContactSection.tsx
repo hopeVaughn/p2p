a4d9dd8c3e80ee275e28602f9c89ba88080f4d4b
hopeVaughn <hopevaughnwarren@gmail.com>
2023-10-05 13:29:54 -0700

    update contactsection

--- apps/client/src/Pages/Components/ContactSection.tsx
+++ apps/client/src/Pages/Components/ContactSection.tsx
@@ -1,113 +1,83 @@
 
-interface ContactInfo {
-  title: string;
-  email?: string;
-  phoneNumber?: string;
-  address?: string[];
-}
-
-const contacts: ContactInfo[] = [
-  {
-    title: 'Collaborate',
-    email: 'collaborate@example.com',
-    phoneNumber: '+1 (555) 905-2345',
-  },
-  {
-    title: 'Press',
-    email: 'press@example.com',
-    phoneNumber: '+1 (555) 905-3456',
-  },
-  {
-    title: 'Join our team',
-    email: 'careers@example.com',
-    phoneNumber: '+1 (555) 905-4567',
-  },
-  {
-    title: 'Say hello',
-    email: 'hello@example.com',
-    phoneNumber: '+1 (555) 905-5678',
-  },
-];
-
-// const locations: ContactInfo[] = [
-//   {
-//     title: 'Los Angeles',
-//     address: ['4556 Brendan Ferry', 'Los Angeles, CA 90210'],
-//   },
-//   {
-//     title: 'New York',
-//     address: ['886 Walter Street', 'New York, NY 12345'],
-//   },
-//   {
-//     title: 'Toronto',
-//     address: ['7363 Cynthia Pass', 'Toronto, ON N3Y 4H8'],
-//   },
-//   {
-//     title: 'Chicago',
-//     address: ['726 Mavis Island', 'Chicago, IL 60601'],
-//   },
-// ];
-
 export default function ContactSection() {
   return (
-    <section className="py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <article className="mx-auto max-w-2xl space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
-          <header className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
-            <div>
-              <h2 className="text-3xl font-bold tracking-tight text-cyan-900">Get in touch</h2>
-              <p className="mt-4 leading-7 text-gray-600">
-                Quam nunc nunc eu sed. Sed rhoncus quis ultricies ac pellentesque.
-              </p>
+    <div className="relative isolate">
+      <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
+        <div className="relative px-6 pb-10 pt-16 sm:pt-324 lg:static lg:px-8 lg:py-32">
+          <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg flex flex-col justify-center items-center h-full">
+            <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
+              <svg
+                className="absolute inset-0 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
+                    width={200}
+                    height={200}
+                    x="100%"
+                    y={-1}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <path d="M130 200V.5M.5 .5H200" fill="none" />
+                  </pattern>
+                </defs>
+                <rect width="100%" height="100%" strokeWidth={0} fill="white" />
+                <svg x="100%" y={-1} className="overflow-visible fill-gray-50">
+                  <path d="M-470.5 0h201v201h-201Z" strokeWidth={0} />
+                </svg>
+                <rect width="100%" height="100%" strokeWidth={0} fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
+              </svg>
             </div>
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
-              {contacts.map((contact) => (
-                <div key={contact.title} className="rounded-2xl bg-gray-50 p-10">
-                  <h3 className="text-base font-semibold leading-7 text-gray-900">{contact.title}</h3>
-                  <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
-                    {contact.email && (
-                      <div>
-                        <dt className="sr-only">Email</dt>
-                        <dd>
-                          <a className="font-semibold text-cyan-600" href={`mailto:${contact.email}`}>
-                            {contact.email}
-                          </a>
-                        </dd>
-                      </div>
-                    )}
-                    {contact.phoneNumber && (
-                      <div className="mt-1">
-                        <dt className="sr-only">Phone number</dt>
-                        <dd>{contact.phoneNumber}</dd>
-                      </div>
-                    )}
-                  </dl>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900">Get in touch</h2>
+            <p className="mt-6 text-lg leading-8 text-cyan-900">
+              We want to hear about your experiences, idea's, and suggestion. Please feel free to drop us a line about what it was like for you to use our app!
+            </p>
+          </div>
+        </div>
+        <form action="#" method="POST" className="px-6 pb-12 pt-16 sm:pb-24 lg:px-8 lg:py-32">
+          <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div className="sm:col-span-2">
+                <label htmlFor="email" className="block text-sm font-semibold leading-6 text-cyan-900">
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+                  />
                 </div>
-              ))}
-            </div>
-          </header>
-          {/* <div className="grid grid-cols-1 gap-x-8 gap-y-10 pt-16 lg:grid-cols-3">
-            <div>
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900">Locations</h2>
-              <p className="mt-4 leading-7 text-gray-600">
-                Consequat sunt cillum cillum elit sint. Qui occaecat nisi in ipsum commodo.
-              </p>
-            </div>
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
-              {locations.map((location) => (
-                <div key={location.title} className="rounded-2xl bg-gray-50 p-10">
-                  <h3 className="text-base font-semibold leading-7 text-gray-900">{location.title}</h3>
-                  <address className="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
-                    {location.address?.map((line) => (
-                      <p key={line}>{line}</p>
-                    ))}
-                  </address>
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="message" className="block text-sm font-semibold leading-6 text-cyan-900">
+                  Message
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows={4}
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+                    defaultValue={''}
+                  />
                 </div>
-              ))}
+              </div>
             </div>
-          </div> */}
-        </article>
+            <div className="mt-8 flex justify-end">
+              <button
+                type="submit"
+                className="rounded-md bg-cyan-700 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+              >
+                Send message
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-    </section>
+    </div>
   );
 }
