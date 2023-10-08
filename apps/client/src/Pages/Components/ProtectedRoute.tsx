365596025ac5936f519073cecf4890cd24da09a9
hopeVaughn <hopevaughnwarren@gmail.com>
2023-10-08 14:37:24 -0700

    update pr import declaration

--- apps/client/src/Pages/Components/ProtectedRoute.tsx
+++ apps/client/src/Pages/Components/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import { PageNotFound } from "..";
 import { useAuth } from "../../utils/hooks";
 import { accessTokenExpired, decodeAccessToken } from "../../utils/helpers";
-import { Dashboard } from './';
+import { Dashboard } from '.';
 const ProtectedRoute = ({ requiredRoles }: { requiredRoles?: string[]; }) => {
   const { isAuthenticated, refreshToken } = useAuth();
   const [loading, setLoading] = useState(true);
