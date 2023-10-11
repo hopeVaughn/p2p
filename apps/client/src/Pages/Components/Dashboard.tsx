9fcb661e437bcf19334fb9052b2a46d03c0f59a0
hopeVaughn <hopevaughnwarren@gmail.com>
2023-10-11 10:56:31 -0700

    update dashboard component

--- apps/client/src/Pages/Components/Dashboard.tsx
+++ apps/client/src/Pages/Components/Dashboard.tsx
@@ -32,10 +32,11 @@ export default function Dashboard({ children }: DashboardProps) {
     imageUrl:
       'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   };
-  const navigation = [
+
+  const navigation = useMemo(() => [
     { name: 'Search', href: '#', current: true },
     { name: 'Add Bathroom', href: '#', current: false },
-  ];
+  ], []);
 
   const userNavigation = [
     { name: 'Your Profile', href: '#' },
