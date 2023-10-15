596125a18b3c162d742e8a0f7002fbb138259bca
hopeVaughn <hopevaughnwarren@gmail.com>
2023-10-15 08:05:17 -0700

    update cors for development

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api'); // <--- this line set the route prefix
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
