9b8be4116ce6c365e5154aeb66dfda438bd11b74
hopeVaughn <hopevaughnwarren@gmail.com>
2023-10-31 12:08:26 -0700

    remove extensions from main.ts in client for build

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { MapContextProvider } from './utils/context/MapContextProvider.tsx';
+import { MapContextProvider } from './utils/context/MapContextProvider';
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
